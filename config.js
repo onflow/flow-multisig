faa9466747ab5483ed2d02a94b27c815a51c9055
justin <justin.barry@dapperlabs.com>
2022-02-04 13:21:31 -0800

    Add missing packages, add vercel cli to run dev env

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 import { config } from "@onflow/fcl";
+import { send } from "@onflow/transport-http";
 
 config({
-  "accessNode.api": "https://access-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
+  "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
   "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
+  "sdk.transport": send,
 });
