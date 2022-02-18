99cb401fe2848ab7e4fbfac0b2cd500f0f89b9e3
Tom Haile <bradford.haile@gmail.com>
2022-02-18 12:06:42 -0600

    add fungible token addresses to config

--- config.js
+++ config.js
@@ -7,11 +7,13 @@ const envSettings = {
     "accessNode.api": "https://access-mainnet-beta.onflow.org",
     "discovery.wallet": "https://fcl-discovery.onflow.org/authn",
     "sdk.transport": grpcSend,
+    "0xFUNGIBLETOKENADDRESS": "0xf233dcee88fe0abe",
   },
   testnet: {
     "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
     "discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn", // Mainnet: "https://fcl-discovery.onflow.org/authn"
     "sdk.transport": httpSent,
+    "0xFUNGIBLETOKENADDRESS": "0x9a0766d93b6608b7"
   },
   testnetLedger: {
     "discovery.wallet": "https://fcl-ledger-web-git-authn-authz-locked-acct-onflow.vercel.app/testnet/authn"
