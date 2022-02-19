5dea32ddaf7441f9ca0e620f50e7e235f5a27384
Tom Haile <bradford.haile@gmail.com>
2022-02-18 18:17:56 -0600

    fix formatting cdc

--- utils/payloads.js
+++ utils/payloads.js
@@ -3,20 +3,20 @@ export const CadencePayloadTypes = {
 }
 export const CadencePayloads = {
     [CadencePayloadTypes.TransferEscrow]: 
-    `import FungibleToken from 0xFUNGIBLETOKENADDRESS
-    transaction(amount: UFix64, to: Address) {
-    let vault: @FungibleToken.Vault
-    prepare(signer: AuthAccount) {
-    self.vault <- signer
-    .borrow<&{FungibleToken.Provider}>(from: /storage/flowTokenVault)!
-    .withdraw(amount: amount)
-    }
-    execute {
-    getAccount(to)
-    .getCapability(/public/flowTokenReceiver)!
-    .borrow<&{FungibleToken.Receiver}>()!
-    .deposit(from: <-self.vault)
-    }
-    }`
+`import FungibleToken from 0xFUNGIBLETOKENADDRESS
+transaction(amount: UFix64, to: Address) {
+let vault: @FungibleToken.Vault
+prepare(signer: AuthAccount) {
+self.vault <- signer
+.borrow<&{FungibleToken.Provider}>(from: /storage/flowTokenVault)!
+.withdraw(amount: amount)
+}
+execute {
+getAccount(to)
+.getCapability(/public/flowTokenReceiver)!
+.borrow<&{FungibleToken.Receiver}>()!
+.deposit(from: <-self.vault)
+}
+}`
 
 }
