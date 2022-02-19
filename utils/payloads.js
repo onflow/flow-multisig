f58a4c3008b1a6de4f5c1ae60e10d9343c04dbb7
Tom Haile <bradford.haile@gmail.com>
2022-02-18 18:13:35 -0600

    update to use transfer token cdc

--- utils/payloads.js
+++ utils/payloads.js
@@ -2,11 +2,21 @@ export const CadencePayloadTypes = {
     TransferEscrow: "Transfer Escrow"
 }
 export const CadencePayloads = {
-    [CadencePayloadTypes.TransferEscrow]: `
-transaction() {
-    prepare(acct: AuthAccount) {
-      log("hello world")
+    [CadencePayloadTypes.TransferEscrow]: 
+    `import FungibleToken from 0xFUNGIBLETOKENADDRESS
+    transaction(amount: UFix64, to: Address) {
+    let vault: @FungibleToken.Vault
+    prepare(signer: AuthAccount) {
+    self.vault <- signer
+    .borrow<&{FungibleToken.Provider}>(from: /storage/flowTokenVault)!
+    .withdraw(amount: amount)
     }
-  }`
+    execute {
+    getAccount(to)
+    .getCapability(/public/flowTokenReceiver)!
+    .borrow<&{FungibleToken.Receiver}>()!
+    .deposit(from: <-self.vault)
+    }
+    }`
 
 }
