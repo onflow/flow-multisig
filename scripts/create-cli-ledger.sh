e36733ec3a39d066204b409eae91761f05abca5f
Tom Haile <bradford.haile@gmail.com>
2022-02-24 09:16:49 -0600

    convenient scripts for creating state, blocto script is having issues with blocto public key, tx fails

---
+++ scripts/create-cli-ledger.sh
@@ -0,0 +1,28 @@
+ACCT=authorizer
+echo "Creating account with cli and ledger keys"
+echo "Creating tx using authorizer account in flow.json"
+flow transactions build ./flow-multisig/scripts/testnet-acct-creation.cdc \
+  --network testnet \
+  --args-json "$(cat "./flow-multisig/scripts/args-cli-ledger.json")" \
+  --proposer $ACCT \
+  --proposer-key-index 0 \
+  --authorizer $ACCT \
+  --payer $ACCT \
+  --gas-limit 1000 \
+  --yes \
+  --output json \
+  -x payload \
+  --save ./flow-multisig/scripts/create-account.rlp
+sleep 1
+echo "signing tx"
+flow transactions sign ./flow-multisig/scripts/create-account.rlp \
+  --signer $ACCT \
+  --filter payload \
+  --yes \
+  --save ./flow-multisig/scripts/create-account-signed.rlp
+sleep 1
+echo "sending tx ..."
+./flow-multisig/scripts/testnet-send.sh
+echo "look in tx-output.json for created account"
+echo "if you have jq use `cat tx-output.json | jq '.events[5].values'`"
+cat tx-output.json | jq '.events[5].values'
--- scripts/testnet-sign.sh
+++
@@ -1,5 +0,0 @@
-flow transactions sign ./flow-multisig/scripts/create-account.rlp \
-  --signer $ACCT \
-  --filter payload \
-  --yes \
-  --save ./flow-multisig/scripts/create-account-signed.rlp
