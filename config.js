ed424c9ada4937663741ff74b4e48a9f05fdba11
Tom Haile <bradford.haile@gmail.com>
2022-03-22 13:18:39 -0500

    fix config url

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 import { config } from "@onflow/fcl";
 import { send as httpSent } from "@onflow/transport-http";
-import { send as grpcSend } from "@onflow/transport-grpc";
+//import { send as grpcSend } from "@onflow/transport-grpc";
 
 const envSettings = {
   mainnet: {
-    "accessNode.api": "https://access-mainnet-beta.onflow.org",
+    "accessNode.api": "https://rest-mainnet.onflow.org",
     "discovery.wallet": "https://fcl-ledger-web-git-authn-authz-locked-acct-onflow.vercel.app/mainnet/authn",
-    "sdk.transport": grpcSend,
+    "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0xf233dcee88fe0abe",
   },
   testnet: {
-    "accessNode.api": "https://access-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
+    "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
     "discovery.wallet": "https://fcl-ledger-web-git-authn-authz-locked-acct-onflow.vercel.app/testnet/authn",
-    "sdk.transport": grpcSend,
+    "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0x9a0766d93b6608b7"
   },
 };
 
-export const setupConfig = (env) => {
+export const setupConfig = (env = "mainnet") => {
   if (envSettings[env]) {
     config({ ...envSettings[env] })
   }
