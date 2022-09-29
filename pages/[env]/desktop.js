f6ad1ced67821965e8d259746b20da3ab680224f
Tom Haile <bradford.haile@gmail.com>
2022-09-29 10:17:09 -0500

    add login, testing gcp-kms wallet integration, checkpoint

--- pages/[env]/desktop.js
+++ pages/[env]/desktop.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useReducer, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
+import * as fcl from "@onflow/fcl"
 import {
-    Tab,
+    Header,
     Tabs,
     TabPanel,
     TabPanels,
@@ -22,45 +23,76 @@ import {
     Grid,
     GridItem
 } from "@chakra-ui/react";
+import { send as httpSent } from "@onflow/transport-http";
 
 export default function Desktop() {
     const [txs, setTxs] = useState(['bob', 'sue']);
+    const [signed, setSigned] = useState(['done one', 'done two'])
     const [selectedTx, setSelectedTx] = useState(null);
     const [publicKey, setPublicKey] = useState(null);
 
-    useEffect(() => {
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    useEffect(async () => {
+        fcl.config({
+            "accessNode.api": "https://rest-mainnet.onflow.org",
+            "discovery.wallet": "http://localhost:3001/local/authn",
+            "sdk.transport": httpSent,
+            "0xFUNGIBLETOKENADDRESS": "0xf233dcee88fe0abe",
+            "0xFLOWTOKENADDRESS": "0x1654653399040a61",
+            "app.detail.icon": "https://flow-multisig-git-service-account-onflow.vercel.app/icon.png",
+            "app.detail.title": "Multisig Webapp",
+          })
+        console.log('set new discovery wallet ')
     }, [])
 
+    const login = async () => {
+        console.log('logging in')
+        try {
+            fcl.logIn()
+        }catch(e){
+            console.log(e)
+        }
+        
+    }
+
     return (
-        <Stack height={"100%"}>
+        <Stack margin="0.25rem" height={"99vh"} overflowY="hidden">
             <Grid
                 templateAreas={`"header header"
                   "nav main"
                   "done main"`}
-                gridTemplateRows={'50px *'}
+                gridTemplateRows={'50px 50% 1fr'}
                 gridTemplateColumns={'1fr 6fr'}
-                gap='2'
+                gap='1'
                 color='blackAlpha.700'
                 fontWeight='bold'
             >
-                <GridItem area={'header'} padding=".5rem">
-                    <Button>{publicKey ? `Logged In` : `Login`}</Button>
+                <GridItem pl='2' area={'header'} padding=".5rem">
+                    <Button onClick={() => login()}>{publicKey ? `Logged In` : `Login`}</Button>
                 </GridItem>
-                <GridItem height="95vh" bg='blue.100' area={'nav'}>
+                <GridItem pl='2' height="95vh" bg='blue.100' area={'nav'}>
                     <Stack padding={"1rem"}>
-                        {txs.length === 0 && <Text>No Transactions</Text>}
+                        <Heading size="md">PENDING</Heading>
+                        {txs.length === 0 && <Heading size="md">NO TRANSACTIONS</Heading>}
                         {txs.length > 0 && txs.map((tx) =>
                             <Stack key={tx}>
-                                <Button disabled={tx === selectedTx} onClick={() => setSelectedTx(tx)}>{tx}</Button>
+                                <Button height="1.5rem" disabled={tx === selectedTx} onClick={() => setSelectedTx(tx)}>{tx}</Button>
                             </Stack>)
                         }
                     </Stack>
                 </GridItem>
-                <GridItem bg='blue.100' area={"done"}>Done</GridItem>
-                <GridItem bg='blue.100' area={'main'}>
+                <GridItem pl='2' bg='blue.100' area={'main'} rowSpan={2}>
                     <Stack>main</Stack>
                 </GridItem>
+                <GridItem pl='2' bg='blue.100' area={"done"}>
+                    <Stack padding={"1rem"}>
+                    <Heading justifyContent={"center"} width="100%" size="md">SIGNED</Heading>
+                    {signed.length > 0 && signed.map(s =>
+                        <Stack key={s}><Text>{s}</Text></Stack>)
+                    }
+                    </Stack>
+                </GridItem>
+
             </Grid>
         </Stack>
     )
