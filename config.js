e89ba4d1431834440203c460f2497b81c9a54257
Tom Haile <bradford.haile@gmail.com>
2022-10-03 20:51:58 -0500

    use new version of multi-account flip 200 fcl-ledger-web

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ const envSettings = {
     "accessNode.api": "https://rest-mainnet.onflow.org",
     //"discovery.wallet": "https://fcl-discovery.onflow.org/mainnet/authn",
     //"discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/mainnet/authn",
-    "challenge.handshake": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/mainnet/authn",
+    "challenge.handshake": "https://fcl-ledger-jp5m8fua2-onflow.vercel.app//mainnet/authn",
     "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0xf233dcee88fe0abe",
     "0xFLOWTOKENADDRESS": "0x1654653399040a61",
@@ -28,7 +28,7 @@ const envSettings = {
     "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
     //"discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
     //"discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/testnet/authn",
-    "challenge.handshake": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/testnet/authn",
+    "challenge.handshake": "https://fcl-ledger-jp5m8fua2-onflow.vercel.app/testnet/authn",
     "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0x9a0766d93b6608b7",
     "0xFLOWTOKENADDRESS": "0x7e60df042a9c0868",
