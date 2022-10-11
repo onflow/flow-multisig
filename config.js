de88ecab8b52a6f98919b9642f7b22d33baea720
Tom Haile <bradford.haile@gmail.com>
2022-10-11 14:27:34 -0500

    support flow ledger app version 0.11.0

--- config.js
+++ config.js
@@ -2,22 +2,12 @@ import { config } from "@onflow/fcl";
 import { send as httpSent } from "@onflow/transport-http";
 //import { send as grpcSend } from "@onflow/transport-grpc";
 
-const ledgerSettings = {
-  mainnet: {
-    "discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/mainnet/authn",
-  },
-  testnet: {
-    "discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/testnet/authn",
-  },
-};
-
-
 const envSettings = {
   mainnet: {
     "accessNode.api": "https://rest-mainnet.onflow.org",
     //"discovery.wallet": "https://fcl-discovery.onflow.org/mainnet/authn",
     //"discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/mainnet/authn",
-    "challenge.handshake": "https://fcl-ledger-jp5m8fua2-onflow.vercel.app//mainnet/authn",
+    "challenge.handshake": "https://fcl-ledger-multisig.vercel.app/mainnet/authn",
     "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0xf233dcee88fe0abe",
     "0xFLOWTOKENADDRESS": "0x1654653399040a61",
@@ -28,7 +18,7 @@ const envSettings = {
     "accessNode.api": "https://rest-testnet.onflow.org", // Mainnet: "https://access-mainnet-beta.onflow.org"
     //"discovery.wallet": "https://fcl-discovery.onflow.org/testnet/authn",
     //"discovery.wallet": "https://fcl-ledger-web-git-multi-account-onflow.vercel.app/testnet/authn",
-    "challenge.handshake": "https://fcl-ledger-jp5m8fua2-onflow.vercel.app/testnet/authn",
+    "challenge.handshake": "https://fcl-ledger-multisig.vercel.app/testnet/authn",
     "sdk.transport": httpSent,
     "0xFUNGIBLETOKENADDRESS": "0x9a0766d93b6608b7",
     "0xFLOWTOKENADDRESS": "0x7e60df042a9c0868",
