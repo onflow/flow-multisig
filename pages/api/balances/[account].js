df7766385cf74baecf2a7ecf431d1d2d73acd9ab
Tom Haile <bradford.haile@gmail.com>
2022-12-19 15:27:22 -0600

    add contracts deployed

--- pages/api/balances/[account].js
+++ pages/api/balances/[account].js
@@ -1,12 +1,18 @@
 import * as fcl from '@onflow/fcl';
 import { send as httpSent } from '@onflow/transport-http';
 
+const TOP_SHOTS = `
+import TopShot from 0xTOPSHOT
+
+
+`;
 const mainnet = {
   'accessNode.api': 'https://rest-mainnet.onflow.org',
   'discovery.wallet': 'https://fcl-ledger-multisig.vercel.app/mainnet/authn',
   'sdk.transport': httpSent,
   '0xFUNGIBLETOKENADDRESS': '0xf233dcee88fe0abe',
   '0xFLOWTOKENADDRESS': '0x1654653399040a61',
+  '0xTOPSHOT': '0x0b2a3299cc857e29',
   'app.detail.icon':
     'https://flow-multisig-git-service-account-onflow.vercel.app/icon.png',
   'app.detail.title': 'Multisig Webapp',
@@ -23,8 +29,12 @@ export default async function handler({ body, method, query }, res) {
         });
       }
       let flow = '0';
+      let contracts = '0';
       try {
         const acct = await fcl.account(account);
+        contracts = acct.contracts?.names?.length
+          ? acct.contracts?.names?.length
+          : 0;
         if (!acct) {
           return res.status(404).json({
             error: 'Invalid account address',
@@ -40,6 +50,7 @@ export default async function handler({ body, method, query }, res) {
       return res.status(200).json({
         data: {
           flow,
+          contracts,
         },
       });
 
