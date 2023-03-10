eed0c4cc3ee63a38d0ee47cd4951ee1da3a0ff8e
Tom Haile <bradford.haile@gmail.com>
2023-03-10 11:01:36 -0600

    fix missing key

--- components/KeysTableSelector.js
+++ components/KeysTableSelector.js
@@ -12,14 +12,15 @@ export const KeysTableSelector = ({ keys, selectedKey, setKey }) => {
 
     return (
         <Table variant='simple' size='sm'>
-            <Tbody><Tr><Td>KeyId</Td><Td>Weight</Td><Td>Public Key</Td></Tr></Tbody>
-            {keys.map(key => {
-                return (<Tr cursor={"pointer"} backgroundColor={selectedKey === key.index ? 'lightGreen' : 'white'} key={`tr-${key.index}`} onClick={() => selectKey(key.index)} >
+            <Tbody><Tr><Td>KeyId</Td><Td>Weight</Td><Td>Public Key</Td></Tr>
+            {keys.map((key, i) => {
+                return (<Tr cursor={"pointer"} backgroundColor={selectedKey === key.index ? 'lightGreen' : 'white'} key={`tr-${key.index}-${i}`} onClick={() => selectKey(key.index)} >
                     <Td key={key.index}>{key.index}</Td>
                     <Td>{key.weight}</Td>
                     <Td>{abbrvKey(key.publicKey)}</Td>
                 </Tr>)
             })}
+            </Tbody>
         </Table>
     )
 }
